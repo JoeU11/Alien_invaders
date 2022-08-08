c2118c2d71bb5142ecdbfe507ff59aa0ad7bbe7b
joe <joe.ulica@gmail.com>
2022-08-08 16:44:05 -0400

    Add removal of laser on collision with target

--- first_game.rb
+++ first_game.rb
@@ -29,7 +29,7 @@ class Player
 end
 
 class Laser
-  attr_reader :y
+  attr_reader :y, :x
   def initialize(player_y, player_x)
     @image = Gosu::Image.new("media/laser.png")
     @y = player_y
@@ -44,9 +44,18 @@ class Laser
     @y -= 10 
   end
 
-  def hit_target(aliens)
-    aliens.reject! {|alien| Gosu.distance(@x, @y, alien.x, alien.y) < 25}
-
+  def hit_target(aliens) 
+    hit = false
+    index = 0
+    while index < aliens.length do
+      if Gosu.distance(@x, @y, aliens[index].x, aliens[index].y) < 25
+        aliens.delete_at(index)
+        hit = true
+        index -= 1
+      end
+      index += 1
+    end
+    return hit
   end
 end
 
@@ -77,7 +86,7 @@ class Invaders < Gosu::Window
 
     @aliens = []
     @player = Player.new
-    @laser = []
+    @lasers = []
     @cooldown = 0
     @alien_count = 0
     @alien_x = 900
@@ -85,6 +94,7 @@ class Invaders < Gosu::Window
   end
 
   def update 
+    @laser_counter = 0
     if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
       if @player.x > 25
         @player.move_left
@@ -97,26 +107,33 @@ class Invaders < Gosu::Window
     end
     if Gosu.button_down? Gosu::KbSpace
       if @cooldown <= 0
-        @laser << Laser.new(@player.y, @player.x)
+        @lasers << Laser.new(@player.y, @player.x)
         @cooldown = 10
       end
     end
     @cooldown -= 1
-    @laser.each {|laser| laser.move}
-    @laser.reject! {|laser| laser.y == 0}
+    @lasers.each {|laser| laser.move}
+    @lasers.reject! {|laser| laser.y == 0}
 
     while @alien_count < 15 #Add row variable 
       @aliens.push(Alien.new(@alien_x, @alien_y))
       @alien_x -= 60
       @alien_count += 1
     end
-    @laser.each {|laser| laser.hit_target(@aliens)}
+    while @laser_counter < @lasers.length
+      if @lasers[@laser_counter].hit_target(@aliens)
+        @lasers.delete_at(@laser_counter)
+        @laser_counter -= 1
+        # Add cue to draw explosion animation here
+      end
+      @laser_counter += 1
+    end
   end
 
   def draw
     @player.draw
     @background_image.draw(0, 0, 0, 1.9, 1.5)
-    @laser.each {|laser| laser.draw}
+    @lasers.each {|laser| laser.draw}
     @font.draw_text("Score: \n\n\n Level: ", 1000, 70, 1, scale_x = 1, scale_y = 1, color = 0xff_ffffff, mode = :default)
     @score_border.draw(980, 40, 1,)
     @aliens.each {|alien| alien.draw}
